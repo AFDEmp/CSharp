ce4352db7151235af194ba493dac3ee1ed237845
fedonman <fedonman@gmail.com>
2018-03-20 00:03:54 +0200

    adds Theory Examples
    1. const vs readonly vs static readonly
    2. pass by value vs pass by reference

---
+++ CB4_MMa18/Theory Examples/const_vs_readonly_vs_staticreadonly/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace const_vs_readonly_vs_staticreadonly
+{
+    class Keywords
+    {
+        /*
+        1. const
+        2. readonly
+        3. static readonly
+        These are very common keywords and are quite confusing.
+        Let's find out the differences.
+        */
+
+        /*
+        'const' is nothing but "constant", a variable of which the value is constant but at compile time. 
+        And it's mandatory to assign a value to it. 
+        By default a const is static and we cannot change the value of a const variable throughout the entire program.
+         */
+        public const string constName = "John";
+
+        /*
+        'readonly' is the keyword whose value we can change during runtime or we can assign it at run time but only through the non-static constructor. Not even a method.
+        */
+        public readonly string readonlyName = "John";
+
+        /*
+        A 'static readonly' type variable's value can be assigned at runtime or assigned at compile time and changed at runtime. In runtime, the variable's value can only be changed once in the static constructor.
+        */
+        public static readonly string staticReadonlyName = "John";
+
+        // normal constructor
+        public Keywords(string name) {
+            // Changing 'const' is not allowed
+            //constName = name;
+
+            // Changing 'readonly' is allowed
+            readonlyName = name;
+
+            // Changing 'static readonly' is not allowed
+            //staticReadonlyName = name;
+        }
+
+        // static constructor
+        static Keywords() {
+            // Changing 'const' is not allowed
+            //constName = "Mary";
+
+            // Changing 'readonly' is not allowed
+            //readonlyName = "Mary";
+
+            // Changing 'static readonly' is allowed
+            staticReadonlyName = "Mary";
+        }
+
+        public void MyMethod() {
+            // Changing 'const' is not allowed
+            //constName = "Mary";
+
+            // Changing 'readonly' is not allowed
+            //readonlyName = "Mary";
+
+            // Changing 'static readonly' is not allowed
+            //staticReadonlyName = "Mary";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Keywords key_01 = new Keywords("Bill");
+            Keywords key_02 = new Keywords("Nick");
+
+            Console.WriteLine($"const: {Keywords.constName}\treadonly: {key_01.readonlyName}\tstatic readonly: {Keywords.staticReadonlyName}");
+
+            Console.WriteLine($"const: {Keywords.constName}\treadonly: {key_02.readonlyName}\tstatic readonly: {Keywords.staticReadonlyName}");
+
+            // Changing 'const' is not allowed
+            //Keywords.constName = "Bo";
+
+            // Changing 'readonly' is not allowed
+            //key_01.readonlyName = "Bo";
+
+            // Changing 'static readonly' is not allowed
+            //Keywords.staticReadonlyName = "Mary";
+        }
+    }
+}
---
+++ CB4_MMa18/Theory Examples/value_vs_reference/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace value_vs_reference
+{
+    class Program
+    {
+        // reference type
+        class TheClass
+        {
+            public string willIChange;
+        }
+
+        // value type
+        struct TheStruct
+        {
+            public string willIChange;
+        }
+
+        class TestClassAndStruct
+        {
+            static void ClassTaker(TheClass c)
+            {
+                c.willIChange = "Changed";
+                Console.WriteLine($"Class field inside method: {c.willIChange}");
+            }
+
+            static void StructTaker(TheStruct s)
+            {
+                s.willIChange = "Changed";
+                Console.WriteLine($"Struct field inside method: {s.willIChange}");
+            }
+
+            static void StringTaker(string s) 
+            {
+                s = "Changed";
+                Console.WriteLine($"String inside method: {s}");
+            }
+
+
+            // If we want to pass and a value-type by reference
+            // then we use the keyword 'ref'
+            static void intTaker(ref int i)
+            {
+                i = 1;
+                Console.WriteLine($"Integer inside method: {i}");
+            }
+
+            static void Main()
+            {
+                TheClass testClass = new TheClass();        //reference type
+                TheStruct testStruct = new TheStruct();     //value type
+                string testString;                          //value type
+                int testInteger;                            //value type
+
+                testClass.willIChange = "Initial";
+                testStruct.willIChange = "Initial";
+                testString = "Initial";
+                testInteger = 0;
+
+                Console.WriteLine($"Class field before method: {testClass.willIChange}");
+                Console.WriteLine($"Struct field before method: {testStruct.willIChange}");
+                Console.WriteLine($"String before method: {testString}");
+                Console.WriteLine($"Integer before method: {testInteger}");
+                Console.WriteLine();
+
+                ClassTaker(testClass);
+                StructTaker(testStruct);
+                StringTaker(testString);
+                // we need to use 'ref' here too!
+                intTaker(ref testInteger);
+                
+                Console.WriteLine();
+                Console.WriteLine($"Class field after method: {testClass.willIChange}");
+                Console.WriteLine($"Struct field after method: {testStruct.willIChange}");
+                Console.WriteLine($"String after method: {testString}");
+                Console.WriteLine($"Integer after method: {testInteger}");
+                Console.WriteLine();
+
+                // Keep the console window open in debug mode.
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+        /* Output:
+            Class field before method: Initial
+            Struct field before method: Initial
+            String before method: Initial
+            Integer before method: 0
+
+            Class field inside method: Changed
+            Struct field inside method: Changed
+            String inside method: Changed
+            Integer inside method: 1
+
+            Class field after method: Changed
+            Struct field after method: Initial
+            String after method: Initial
+            Integer after method: 1
+        */
+    }
+}
